ff50ba00a42f47862495ec47e54608217100f825
Christian Hensen <christian.j.hensen@gmail.com>
2017-11-02 20:40:02 -0400

    fix lint errors

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -7,7 +7,7 @@ export default function NavBar(props) {
   const navElements = props.navElements.map((element, index) =>
 
     <li key={index}>
-      <a href="#" className={'nav-item ' + (props.currentView === element ? 'active' : "")} onClick={() => props.changeView(element)}>
+      <a href="" className={'nav-item ' + (props.currentView === element ? 'active' : "")} onClick={() => props.changeView(element)}>
         <div className={'nav-icon ' + element}></div>
         <div className={'nav-text ' + element}>{element}</div>
       </a>
