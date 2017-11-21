5b5d2ac926dbf825baddcce750b41d028ecdcbb0
Christian Hensen <christian.j.hensen@gmail.com>
2017-11-21 18:59:28 -0500

    auth tests

--- src/components/auth/auth.spec.js
+++ src/components/auth/auth.spec.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
-import AuthContainer from './';
+import {AuthContainer} from './';
 
 describe('<Auth />', () => {
   it('renders without crashing', () => {
-    shallow(<AuthContainer authView={'signup'} />);
+    shallow(<AuthContainer />);
   });
 
-  xit('should render the signup component', () => {
+  it('should render the signup component', () => {
     const wrapper = mount(<AuthContainer authView={'signup'} />);
     expect(wrapper.find('#sign-up').length).toBe(1);
   });
 
-  xit('should render the login component', () => {
+  it('should render the login component', () => {
     const wrapper = mount(<AuthContainer authView={'login'} />);
     expect(wrapper.find('#log-in').length).toBe(1);
   });
