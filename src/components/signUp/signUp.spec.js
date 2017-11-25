91aa74c8c46f12e570d3ab7bc00e6bbd6aae4ce3
Christian Hensen <christian.j.hensen@gmail.com>
2017-11-25 12:43:11 -0500

    signup tests

--- src/components/signUp/signUp.spec.js
+++ src/components/signUp/signUp.spec.js
@@ -7,4 +7,9 @@ describe('<SignUp />', () => {
   it('renders without crashing', () => {
     shallow(<SignUp />);
   });
+
+  it('renders the sign up form', () => {
+    const wrapper = mount(<SignUp />);
+    expect(wrapper.find("#sign-up").length).toBe(1);
+  });
 });
