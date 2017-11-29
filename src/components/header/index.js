9a4e0c646a9aa3cdb6deae01cfed6934fd74b438
Christian Hensen <christian.j.hensen@gmail.com>
2017-11-29 14:08:46 -0500

    header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@ import Header from './header';
 
 export default function HeaderContainer(props) {
 
-  if(props.currentView === 'categories' || props.currentView === 'index') {
+  if(props.currentView === 'categories' || props.currentView === 'index' || props.currentView === 'login' || props.currentView === 'signup') {
     return null;
   } else {
     return <Header title={props.currentView} />
